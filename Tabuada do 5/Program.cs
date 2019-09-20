60b8747e5e48bfa6d43233a8b2e1e7028b287b1f
Nicolas Freitas <nsilvafreitas08@gmail.com>
2019-09-20 11:14:28 -0300

    Atualização

---
+++ Tabuada do 5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tabuada_do_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            for (x = 5; x <= 50; x+=5){
+            Console.WriteLine(x+ " ");
+            }
+        
+        }
+    }
+}
