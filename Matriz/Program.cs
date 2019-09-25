cb25f4c8ded1175f8ceb2995de6845caf087c0ad
Nicolas Freitas <nsilvafreitas08@gmail.com>
2019-09-25 11:21:55 -0300

    Atualização

---
+++ Matriz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        Console.WriteLine("Matriz");
+        int[] vetor = {1,2,3,4,5,6};
+
+        for(int i=0; i<7; i++)
+        Console.WriteLine(vetor[i]);
+
+        if (vetor[i] % 2 == 0){
+
+        Console.WriteLine("Esse número é par");
+        }else{
+        Console.WriteLine("Esse número é ímpar");
+        }
+        
+        }
+    }
+}
