09f4f0765b51eb18227b4d4563bec3c99ea5220c
Nicolas Freitas <nsilvafreitas08@gmail.com>
2019-10-30 11:21:43 -0300

    fawifwanj

---
+++ Desafios/Vetores prof/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Vetores_prof
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vetor = new string[5];
+            for(int i = 0; i < 5; i++)
+            {
+                System.Console.WriteLine("Nome: ");
+                vetor[i] = Console.ReadLine();
+            }
+
+            System.Console.WriteLine("Troca: ");
+            string troca = Console.ReadLine();
+            
+            for (int y = 4; y >= 0; y--)
+            {
+                
+                if(vetor[y] == troca)
+                {
+                    System.Console.WriteLine("Novo:");
+                    vetor[y] = troca;
+                    break;
+                }
+                System.Console.WriteLine("Não encontrei o ");
+            }
+        }
+    }
+}
